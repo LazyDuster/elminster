21ee2e73bb6c19c769c2cb24857c7d6e7bbc71ff
Michal Lesniewski <michalklesniewski@gmail.com>
2022-02-13 21:53:53 -0500

    Now works properly with links, and uses new ytdl fork to not get rate limited!

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from requests import get
-import youtube_dl
+from yt_dlp import YoutubeDL
 import discord
 from discord.ext import commands
 import logging
+import asyncio
 
 # bot setup
 print(discord.__version__)
@@ -32,13 +33,13 @@ def download_video(search):
         'verbose': False,
         'logger': ErrorLogging(),
     }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    with YoutubeDL(ydl_opts) as ydl:
         try:
             get(search)
         except:
             video = ydl.extract_info(f"ytsearch:{search}", download=False)['entries'][0]['webpage_url']
         else:
-            video = ydl.extract_info(search, download=False)
+            video = search
         ydl.download([video])
 
 # commands
@@ -52,13 +53,16 @@ async def help(ctx):
     await ctx.send('Begin a request with \'$\' and I will dazzle you with my abilities!')
 
 @bot.command()
-async def play(ctx, search):
+async def play(ctx, *, search):
     download_video(search)
     source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("output.opus"), volume=0.1)
     if ctx.voice_client is None:
         channel = ctx.message.author.voice.channel
         await channel.connect()
     ctx.voice_client.play(source)
+    while ctx.voice_client.is_playing():
+        await asyncio.sleep(.1)
+    await ctx.voice_client.disconnect()
 
 @bot.command()
 async def developers(ctx):
@@ -74,9 +78,9 @@ async def dc(ctx):
     if ctx.voice_client is not None:
         if ctx.voice_client:
             await ctx.guild.voice_client.disconnect()
-            ctx.send('Elminster shall take his leave!')
+            await ctx.send('Elminster shall take his leave!')
         else:
-            ctx.send('Silence knave, I\'m not in a voice channel.')
+            await ctx.send('Silence knave, I\'m not in a voice channel.')
 
 # discord.py logs
 logger = logging.getLogger('discord')
